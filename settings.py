9534c788cf49f9932910580ef4ac99975e9b2e43
apiguy <freedomdumlao@gmail.com>
2014-02-13 09:11:34 -0500

    fixing some config stuff and requirements

--- settings.py
+++ settings.py
@@ -1,13 +1,10 @@
 import os
 get = os.environ.get
 
-
 MEETUP_GROUP_ID = get('MEETUP_GROUP_ID')
 MEETUP_OAUTH_KEY = get('MEETUP_OAUTH_KEY')
 MEETUP_OAUTH_SECRET = get('MEETUP_OAUTH_SECRET')
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://freedom@localhost/studygroup'
-
+SQLALCHEMY_DATABASE_URI = get('SQLALCHEMY_DATABASE_URI')
 DEFAULT_MAX_MEMBERS = 10
 
 
