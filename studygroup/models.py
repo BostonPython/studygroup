bfaeb79eb71b25e32605e5370258d532b88a5dba
Jason Michalski <armooo@armooo.net>
2014-04-14 08:23:25 -0700

    Normalise quotes to single quotes

--- studygroup/models.py
+++ studygroup/models.py
@@ -8,13 +8,14 @@ import settings
 ROLE_MEMBER = 1
 ROLE_GROUP_LEADER = 10
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     meetup_member_id = db.Column(db.String, unique=True)
     full_name = db.Column(db.String)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
-    memberships = db.relationship("Membership", backref="user")
+    memberships = db.relationship('Membership', backref='user')
 
 
 class Membership(db.Model):
@@ -31,7 +32,7 @@ class Group(db.Model):
     max_members = db.Column(db.Integer, nullable=False,
                             default=settings.DEFAULT_MAX_MEMBERS)
 
-    memberships = db.relationship("Membership", backref="group")
+    memberships = db.relationship('Membership', backref='group')
 
     @classmethod
     def all_with_memberships(cls):
