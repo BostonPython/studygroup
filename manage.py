fb0b741ba9d3d1aa06d90c26dae7b11c791e2849
Mike Milkin <mmilkin@shoobx.com>
2014-12-04 20:26:39 -0500

    manager changes

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager
 
-from studygroup.application import create_app, db
+from studygroup.application import create_app
 
-
-migrate = Migrate(create_app)
+manager = Manager(create_app)
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == "__main__":
     manager.run()
